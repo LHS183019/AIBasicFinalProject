742ddc89cda76ed2c9620c691e0d97b5d19b6497
LHS183019 <cindyxusinga@gmail.com>
2025-05-10 18:43:32 +0800

    V0: The very prototype of the project, basically just a simplest framework is implemented.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AIBasicFinalProject
+
+
+你需要在本地根目录配置.env文件
+```
+SILICONFLOW_API_KEY=你的siliconflow_key
+GEMINI_API_KEY=你的GEMINI_key
+OPENAI_AGENTS_DISABLE_TRACING=1
+```
+以及下载需要用到的一些依赖，在你的环境中运行：
+```
+pip install -r requirements.txt
+```
+
+
--- main.py
+++ main.py
@@ -1,88 +1,51 @@
-from openai import OpenAI
-from pydantic import BaseModel
 import keyboard
 import colorama
+import getpass
+import os
+from llm import ask_LLM,ModelType
+from response_handler import process_response
+from dotenv import load_dotenv
+import os
+
 
 """
-TODO:
-1. safety issue
-https://zhuanlan.zhihu.com/p/30480330292
-https://www.promptingguide.ai/zh/risks/adversarial#%E5%8F%82%E6%95%B0%E5%8C%96%E6%8F%90%E7%A4%BA%E7%BB%84%E4%BB%B6
-API key manage
-
-2. CLI?
-https://blog.csdn.net/sunshine_youngforyou/article/details/146162004
-
-3. Agent
-https://zhuanlan.zhihu.com/p/24432308656
-https://zhuanlan.zhihu.com/p/657937696
-大模型 Agent 是不是就是各种 Prompt 的堆叠？ - Tool的回答 - 知乎
-https://www.zhihu.com/question/1894891236617332066/answer/1900585340592424543
-https://zhuanlan.zhihu.com/p/32230066307
-
-4. update requirements.txt
+NOTE:
+每次更新之后都建议update requirements.txt,在工作路径下面执行:
+pipreqs . --encoding=utf8 --force
+你可能需要在当前环境中安装pipreqs这个库:
 pip install pipreqs
-pipreqs . --encoding=utf8
+
+TODO:
+1. 让用户选择模型
+2. 让用户设置自己的API key并缓存到本地
+3. 让用户选择是否启用长思考功能(切换到没有思考能力的模型?)、输出长思考过程
+4. 让用户可以进行多模态输入，接受多模态输出
+5. 结构化输入输出
 """
 
-client = OpenAI(api_key="sk-qseennfhdprismchczwnkzpohyjmuwgpiaywuclsisgugfvo", 
-                base_url="https://api.siliconflow.cn/v1")
 
-                
-def ask_LLM(content):
-    
-    response = client.chat.completions.create(
-        model='Pro/deepseek-ai/DeepSeek-R1',
-        # model="Qwen/Qwen2.5-72B-Instruct",
-        messages=[
-            {'role': 'user', 
-            'content': content}
-        ],
-        stream=True
-    )
-
-    print("输入'q'中止模型输出：")
-    return process_response(response)
-
-def process_response(response):
-    reasoning = False
-    for chunk in response:
-        if not chunk.choices:
-            continue
-        
-        # 检测Ctrl+Q按键事件
-        if keyboard.is_pressed('q'):
-            print("\n模型输出已中止")
-            break
-        
-        if chunk.choices[0].delta.content:
-            if reasoning:
-                reasoning = False
-                print("#"*30)
-            print(chunk.choices[0].delta.content, end="", flush=True)
-        if chunk.choices[0].delta.reasoning_content:
-            if not reasoning:
-                reasoning = True
-                print(f"{'#'*10}思考过程{'#'*10}")
-            print(chunk.choices[0].delta.reasoning_content, end="", flush=True)
-            
-            
+# HACK: 只是一个临时的CLI，UI有待完善
 if __name__ == "__main__":
+    load_dotenv()
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    SILICONFLOW_API_KEY = os.getenv("SILICONFLOW_API_KEY")
 
+    
     print("欢迎来到语言大模型应用！在下方输入您想问大模型的问题。输入'Ctrl+Z'（Windows）或'Ctrl+D'（Unix/Linux/Mac）以退出应用。")
     
     while True:
         try:
-            # 读取用户输入
             user_input = input(">>> ").strip()
-            ask_LLM(user_input)
-        
+            if user_input: # 若输入非空
+                response = ask_LLM(user_input,ModelType.SILICONFLOW_DEEPSEEK_R1,SILICONFLOW_API_KEY)
+                print("输入'q'中止模型输出：")
+                process_response(response,hotkey='q')
         except EOFError:
             # 捕获Ctrl+Z（或Ctrl+D）输入
             print("\n欢迎下次使用！")
             break
         
         except Exception as e:
-            # 捕获并打印其他异常
-            print(f"出现错误: {e}")
+            print(f"{str(e)}")
+            print(f"自动退出程序！")
             break
